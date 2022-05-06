3abc4513b5ec42bc58fcaefe79ef40e1f43b904e
lucakreativ <lucaeckenfels@gmx.de>
2022-05-06 16:29:15 +0200

    finished import

--- import.py
+++ import.py
@@ -2,6 +2,20 @@ import csv
 import mysql.connector
 
 
+state="INSERT INTO schueler (ID, Stufe, Klasse, Vorname, Nachname, Religion, Fremdsp1, Fremdsp2, Fremdsp3) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="user",
+  password="test",
+  database="create_test"
+)
+
+mycursor = mydb.cursor()
+
+
+
 with open("./export.csv", "r") as file:
     csvFile=csv.reader(file)
 
@@ -10,6 +24,9 @@ with open("./export.csv", "r") as file:
 #['Klasse', 'Vornamen', 'Nachname', 'ID', 'Fremdsprache 1.', 'Fremdsprache 2.', 'Fremdsprache 3.', 'Religion', 'Alle besuchten Pflichtfächer']
 #   0           1           2         3             4               5                   6               7                   8
     for lines in csvFile:
+        values=[]
+
+
         klasse=lines[0]
         vorname=lines[1]
         nachname=lines[2]
@@ -29,3 +46,19 @@ with open("./export.csv", "r") as file:
         else:
             stufe=klasse[0:2]
             klasse=""
+
+
+        values.append(Sch_ID)
+        values.append(stufe)
+        values.append(klasse)
+        values.append(vorname)
+        values.append(nachname)
+        values.append(religion)
+        values.append(frem1)
+        values.append(frem2)
+        values.append(frem3)
+
+        print(values)
+
+        mycursor.execute(state, values)
+        mydb.commit()
