82b03cc2b306d9a9aad1f692b4fb6ce7edca97f5
lucakreativ <lucaeckenfels@gmx.de>
2022-05-21 16:48:14 +0200

    function insert book into "ausgeliehen"

--- manage_data.py
+++ manage_data.py
@@ -13,3 +13,16 @@ def insert_book(ISBN, Titel, Verlag, preis=0):
     values=[ISBN, Titel, Verlag, preis]
     mycursor.execute("INSERT INTO buecher (ISBN, Titel, Verlag, preis) VALUES (%s, %s, %s, %s)", values)
     mydb.commit()
+
+def insert_taken_book(Sch_ID, ISBN, Anzahl=1):
+    mycursor.execute("""SELECT Anzahl FROM ausgeliehen WHERE ID='%s' AND ISBN=%s""" % (Sch_ID, ISBN))
+    result=mycursor.fetchall()
+    print(result)
+    if len(result)!=0:
+        books=result[0][0]
+        print(books)
+        mycursor.execute("""UPDATE ausgeliehen SET Anzahl=%s WHERE ID='%s' AND ISBN=%s""" % (books+Anzahl, Sch_ID, ISBN))
+        mydb.commit()
+    else:
+        mycursor.execute("""INSERT INTO ausgeliehen (ID, ISBN, Anzahl) VALUES ('%s', %s, %s)""" % (Sch_ID, ISBN, Anzahl))
+        mydb.commit()
