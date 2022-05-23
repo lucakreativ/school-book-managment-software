98773a3ed0cf01b30a6a12cf4cb200817479f91a
lucakreativ <lucaeckenfels@gmx.de>
2022-05-23 20:18:40 +0200

    absolute and relative amount of books

--- manage_data.py
+++ manage_data.py
@@ -1,3 +1,4 @@
+from unittest import result
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -14,14 +15,25 @@ def insert_book(ISBN, Titel, Verlag, preis=0):
     mycursor.execute("INSERT INTO buecher (ISBN, Titel, Verlag, preis) VALUES (%s, %s, %s, %s)", values)
     mydb.commit()
 
-def insert_taken_book(Sch_ID, ISBN, Anzahl=1):
+
+def insert_taken_book_add(Sch_ID, ISBN, Anzahl=1):
     mycursor.execute("""SELECT Anzahl FROM ausgeliehen WHERE ID='%s' AND ISBN=%s""" % (Sch_ID, ISBN))
     result=mycursor.fetchall()
-    print(result)
     if len(result)!=0:
         books=result[0][0]
-        print(books)
         mycursor.execute("""UPDATE ausgeliehen SET Anzahl=%s WHERE ID='%s' AND ISBN=%s""" % (books+Anzahl, Sch_ID, ISBN))
+        mydb.commit()
+    else:
+        mycursor.execute("""INSERT INTO ausgeliehen (ID, ISBN, Anzahl) VALUES ('%s', %s, %s)""" % (Sch_ID, ISBN, Anzahl))
+        mydb.commit()
+
+
+def insert_taken_book_absolute(Sch_ID, ISBN, Anzahl=1):
+    mycursor.execute("""SELECT Anzahl FROM ausgeliehen WHERE ID='%s' AND ISBN=%s""" % (Sch_ID, ISBN))
+    result=mycursor.fetchall()
+    if len(result)!=0:
+        books=result[0][0]
+        mycursor.execute("""UPDATE ausgeliehen SET Anzahl=%s WHERE ID='%s' AND ISBN=%s""" % (Anzahl, Sch_ID, ISBN))
         mydb.commit()
     else:
         mycursor.execute("""INSERT INTO ausgeliehen (ID, ISBN, Anzahl) VALUES ('%s', %s, %s)""" % (Sch_ID, ISBN, Anzahl))
