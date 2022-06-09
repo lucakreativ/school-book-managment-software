344da87484f1b56f6c003ca1e11055fba5e82a70
lucakreativ <lucaeckenfels@gmx.de>
2022-06-09 17:25:26 +0200

    added crypto to encrypt and decrypt

---
+++ cryption.py
@@ -0,0 +1,40 @@
+import os
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+import base64
+from read_config import read_aes_config
+
+KEYSIZE = 16
+BLOCKSIZE = 16
+plaintext1 = "Hello! Welcome to The Security Buddy!!"
+
+def encrypt(plaintext):
+    config=read_aes_config()
+    key=config["password"]
+    iv=config["iv"]
+    key=key.encode("ascii")
+    iv=iv.encode("ascii")
+
+    cipher1 = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher1.encrypt(pad(plaintext.encode(), BLOCKSIZE))
+
+    ciphertext=(base64.b64encode(ciphertext)).decode("ascii")
+    return ciphertext
+
+def decrypt(ciphertext):
+    config=read_aes_config()
+    key=config["password"]
+    iv=config["iv"]
+    key=key.encode("ascii")
+    iv=iv.encode("ascii")
+
+    ciphertext=ciphertext.encode("ascii")
+    ciphertext=base64.b64decode(ciphertext)
+
+    cipher2 = AES.new(key, AES.MODE_CBC, iv)
+    plaintext2 = unpad(cipher2.decrypt(ciphertext), BLOCKSIZE)
+
+
+    return plaintext2.decode()
+
+print(encrypt("jz7miejYxX70ES89xFfqHex2vCNH3hmzeJ+xwlMpTy0="))
--- read_config.py
+++ read_config.py
@@ -21,3 +21,25 @@ def read_db_config(filename='config.ini', section='mysql'):
         raise Exception('{0} not found in the {1} file'.format(section, filename))
 
     return db       #gibt Dictionary mit den Einträgen zurück
+
+
+def read_aes_config(filename='config.ini', section='aes'):
+    """ Liest die Konfigurationsdatei und gibt ein Dictionary Objekt zurück
+    filename = Dateiname der Konfigurationsdatei
+    section = Teil von der Konfigurationsdatei, der gelesen Werden soll
+    """
+
+    #erstellt den Parser und liest ini Konfigurationsdatei
+    parser = ConfigParser()
+    parser.read(filename)
+
+    #bekommt Sektion, liest mysql-Teil
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db       #gibt Dictionary mit den Einträgen zurück
