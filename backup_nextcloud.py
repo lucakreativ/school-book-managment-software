3c27f535662b71b8d23146b5bb5cf5087952ece1
lucakreativ <lucaeckenfels@gmx.de>
2023-05-08 20:40:05 +0200

    updated backup to requests

--- backup_nextcloud.py
+++ backup_nextcloud.py
@@ -1,8 +1,15 @@
-import owncloud
+import requests
 import time
 import os
 
 from read_config import read_db_config, read_nextcloud_config
+
+
+headers = {
+    "Content-Type": "application/octet-stream",
+    "Depth": "0"
+}
+
 
 
 def export_database():
@@ -13,31 +20,19 @@ def export_database():
     return file, file_path
 
 
-def upload(file, file_path, nextcloud_config, oc):
-    oc.put_file(nextcloud_config["folder"]+file, file_path)
+def upload(file, file_path, nextcloud_config):
+    with open(file_path, "rb") as f:
+        response = requests.put(nextcloud_config["adress"]+"/remote.php/dav/files/"+nextcloud_config["username"]+"/"+nextcloud_config["folder"]+file, auth=(nextcloud_config["username"], nextcloud_config["password"]), headers=headers, data=f)
+        if response.status_code!=201:
+            raise Exception("Es konnte kein Backup hochgeladen werden")
 
-def delte_oldest(nextcloud_config, oc):
-    files=oc.list(nextcloud_config["folder"])
-    data=[]
-    if len(files)>int(nextcloud_config["delete_if_more"]):
-        for f in files:
-            data.append(str(f))
 
-        data.sort(reverse=True)
 
-        to_delete=data[int(nextcloud_config["delete_if_more"]):]
-        for i in to_delete:
-            lower=i.find("path=")+5
-            upper=i.find(",")
-            path=i[lower:upper]
-            oc.delete(path)
 
 def make_backup():
     
     file, file_path=export_database()
 
     nextcloud_config = read_nextcloud_config()
-    oc = owncloud.Client(nextcloud_config["adress"])
-    oc.login(nextcloud_config["username"], nextcloud_config["password"])
-    upload(file, file_path, nextcloud_config, oc)
-    delte_oldest(nextcloud_config, oc)
+    
+    upload(file, file_path, nextcloud_config)
